87da066320efdbee3534ad355a455389af1d9b0d
Yoongdi <101148622+Yoongdi@users.noreply.github.com>
2023-03-27 01:34:14 +0900

    Create HelloWorld.java
    Print an String
    참고로 난 INTP

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter your mbti: ");
+		
+		String mbti = scanner.nextLine();
+				
+		System.out.println("you entered: " + mbti);
+
+	}
+
+}
